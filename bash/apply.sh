eda1b2d0b1e188256fca96248a7db46befea6610
tacobayle <nicolas.bayle@gmail.com>
2022-01-07 09:35:44 +0000

    20220107_apply

---
+++ bash/apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# check if TF is installed
+# check if jq is installed
+echo -n "vsphere server FQDN: "
+read -r vsphere_server
+echo -n "vsphere username: "
+read -r vsphere_username
+echo -n "vsphere password: "
+read -r vsphere_password
