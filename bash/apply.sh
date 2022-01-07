9cb684090085102f21f554b51ac625af93092e93
tacobayle <nicolas.bayle@gmail.com>
2022-01-07 09:48:07 +0000

    20220107_apply

--- bash/apply.sh
+++ bash/apply.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 # check if TF is installed
 # check if jq is installed
+run_cmd() {
+  retry=10
+  pause=20
+  attempt=0
+  echo "############################################################################################"
+  while [ $attempt -ne $retry ]; do
+    if eval "$@"; then
+      echo "$1 PASSED"
+      break
+    else
+      echo "$1 FAILED"
+    fi
+    ((attempt++))
+    sleep $pause
+    if [ $attempt -eq $retry ]; then
+      echo "$1 FAILED after $retry retries" | tee /tmp/cloudInitFailed.log
+      exit 255
+    fi
+    done
+}
 echo -n "vsphere server FQDN: "
 read -r vsphere_server
 echo -n "vsphere username: "
@@ -8,7 +28,6 @@ read -r vsphere_username
 echo -n "vsphere password: "
 read -s vsphere_password
 echo
-curl https://raw.githubusercontent.com/tacobayle/bash/master/vcenter/get_vcenter.sh -o get_vcenter.sh
+run_cmd 'curl https://raw.githubusercontent.com/tacobayle/bash/master/vcenter/get_vcenter.sh -o get_vcenter.sh --silent ; test $(ls -l get_vcenter.sh | awk '"'"'{print $5}'"'"') -gt 0'
 /bin/bash get_vcenter.sh $vsphere_server $vsphere_username $vsphere_password
-
 
