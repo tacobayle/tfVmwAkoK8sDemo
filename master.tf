b2d40c8bc1abe4c016a4a88741d2c015848946e9
tacobayle <nicolas.bayle@gmail.com>
2022-02-22 11:52:49 +0000

    20220222_remove_master_if_name

--- master.tf
+++ master.tf
@@ -162,8 +162,8 @@ resource "null_resource" "update_ip_to_master" {
 
   provisioner "remote-exec" {
     inline = var.vcenter_network_mgmt_dhcp == true ? [
-      "if_secondary_name=$(ip -o link show | awk -F': ' '{print $2}' | grep -v docker  | tail -1)",
-      "sed -i -e \"s/if_name_secondary_to_be_replaced/\"$if_secondary_name\"/g\" /tmp/50-cloud-init.yaml",
+      "if_secondary_name=$(sudo dmesg | grep eth0 | tail -1 | awk -F' ' '{print $5}' | sed 's/://')",
+      "sudo sed -i -e \"s/if_name_secondary_to_be_replaced/\"$if_secondary_name\"/g\" /tmp/50-cloud-init.yaml",
       "sudo cp /tmp/50-cloud-init.yaml ${var.master.net_plan_file}",
       "sudo netplan apply",
       "sleep 10",
@@ -173,8 +173,8 @@ resource "null_resource" "update_ip_to_master" {
       "sudo systemctl daemon-reload",
       "sudo systemctl restart kubelet"
     ] : [
-      "if_secondary_name=$(ip -o link show | awk -F': ' '{print $2}' | grep -v docker  | tail -1)",
-      "sed -i -e \"s/if_name_secondary_to_be_replaced/\"$if_secondary_name\"/g\" /tmp/50-cloud-init.yaml",
+      "if_secondary_name=$(sudo dmesg | grep eth0 | tail -1 | awk -F' ' '{print $5}' | sed 's/://')",
+      "sudo sed -i -e \"s/if_name_secondary_to_be_replaced/\"$if_secondary_name\"/g\" /tmp/50-cloud-init.yaml",
       "sudo netplan apply",
       "sleep 10",
       "sudo cp /etc/systemd/system/kubelet.service.d/10-kubeadm.conf /etc/systemd/system/kubelet.service.d/10-kubeadm.conf.old",
